bf1c476cf36c021bdb646c5c3f97ce30ec1e80e8
Smarika Malviya <114747817+smarika17@users.noreply.github.com>
2024-10-19 21:55:09 +0530

    Create button.jsx

---
+++ button.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+const Test = () => {
+    return(
+        <Button 
+            // Define Button property
+        />
+    )
+}
+export default Test;
